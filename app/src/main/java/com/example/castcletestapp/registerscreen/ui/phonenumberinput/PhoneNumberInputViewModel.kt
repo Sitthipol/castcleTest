733970fdb52ca39d1527d316eca50fd77367f389
Sitthipol (Benz) Janyawarangkul <jan.sitthipol@gmail.com>
2021-11-24 21:12:42 +0700

    init

---
+++ app/src/main/java/com/example/castcletestapp/registerscreen/ui/phonenumberinput/PhoneNumberInputViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.castcletestapp.registerscreen.ui.phonenumberinput
+
+import androidx.lifecycle.ViewModel
+
+class PhoneNumberInputViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
